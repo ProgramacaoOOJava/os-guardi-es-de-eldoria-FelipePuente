79dcf8d31c554ae5ab637b0f5098bb3931cc337e
Felipe Puente <lfpramos@yahoo.com.br>
2026-07-21 02:19:13 +0000

    iniciando

--- Personagem.java
+++ Personagem.java
@@ -7,18 +7,19 @@ public class Personagem {
 
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
+
+    //Método base que retorna a informação comum a todos os personagens
+    public String getDescricaoBase() {
+        return "Nome: " + nome + " | Nível: " + nivel;
+    }    
 
     public void exibirStatus() {
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
     }
 }
 
